e71bf7c38ee197b74e2fee0d25ab00c83bc7b201
kenana-mohammad <kenana4324@gmail.com>
2024-03-17 16:42:13 +0200

    react

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+
+        <div className="container-footer">
+            <div className="row-footer">
+                <div className="col-12-footer">
+                    <p>
+                    Copyright © 2024 focalx .<br/> 
+                    Design By :kenana 
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
